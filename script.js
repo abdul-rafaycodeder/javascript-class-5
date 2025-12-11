e3eda085bbf8df63a41aaab122cc6eca6ffb054c
abdul rafay <abdulrafaycodeder@email.com>
2025-12-11 05:10:28 -0800

    start caseses

---
+++ script.js
@@ -0,0 +1,37 @@
+// var uname = "karachi";
+// var upperName = uname.toUpperCase();
+// console.log(upperName);
+
+// // var city = "KARACHI";
+// // var lowerCity = city.toLowerCase();
+// // console.log(lowerCity);
+
+
+// var city = "KARACHI";
+// var cityup = city.toLowerCase();
+// console.log(cityup)
+
+// var uname = "rafay"
+// console.log(uname.toUpperCase())
+
+
+// let name = "osama";
+// let name_name  = name_name.toUpperCase();
+// console.log(name_name) 
+
+// var mname = "RAFAY";
+// var qname = mname.toLowerCase();
+// console.log(qname);
+
+// var uname = "rafay";
+// uname.toUpperCas();
+// console.log(uname)
+
+
+
+var firstName = "abdul rafay";
+var upper = firstName.toUpperCase();  // ABDUL RAFAY
+var lower = firstName.toLowerCase();  // abdul rafay
+
+console.log(upper);
+console.log(lower);
